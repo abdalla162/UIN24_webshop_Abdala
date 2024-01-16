c83febf2e18f13d82c97702e1a12aaa638b949f4
abdalla162 <abdalla162@hotmail.com>
2024-01-16 15:10:57 +0100

    Lego

--- UIN24_webshop_Abdala-main/shopscript.js
+++ UIN24_webshop_Abdala-main/shopscript.js
@@ -1,1 +1,16 @@
-document.getElementById("test").innerHTML = "Det funker!"
+console.log(products)
+
+let productHTML = "<h2>Ninjago</h2>"
+
+products.map(product => productHTML += `<article>
+<img src="website_images/PROD_${product.imagefile}" alt="${product.title}"/>
+<a href="#">${product.category}</a>
+<h3>${product.title}</h3>
+<span>Kr. ${product.price}</span>
+<button>Legg i handlekurv</button>
+</article>`)
+
+console.log(productHTML)
+
+const main = document.getElementsByTagName("main")
+main[0].innerHTML = productHTML
